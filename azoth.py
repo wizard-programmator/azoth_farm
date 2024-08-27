9d4f5b0124c171cf785e37219428456b9e29b4b5
vincent adeniji <mooncatcherseth@gmail.com>
2024-08-26 21:01:22 -0600

    change realm when happiness is not zero

--- azoth.py
+++ azoth.py
@@ -4,19 +4,26 @@ import logging
 import os
 import time as t
 from pynput.keyboard import Key, Controller
-
+import random
 
 
 # Constants for image file paths based on resolution
 IMAGE_PATHS = {
     "1920x1080": {
-        "scout": "assets/1920x1080/scout.png",
+        "scout1": "assets/1920x1080/scout1.png",
+        "scout2": "assets/1920x1080/scout2.png",
         "pet": "assets/1920x1080/pet.png",
         "snack": "assets/1920x1080/snack.png",
         "food": "assets/1920x1080/food.png",
         "feed": "assets/1920x1080/feed.png",
         "wizard101": "assets/1920x1080/wizard101.png",
-        "close": "assets/1920x1080/close.png"
+        "close": "assets/1920x1080/close.png",
+        "book": "assets/1920x1080/book.png",
+        "option": "assets/1920x1080/option.png",
+        "realms": "assets/1920x1080/realms.png",
+        "next_realm": "assets/1920x1080/next_realm.png",
+        "realm_name": "assets/1920x1080/realm_name.png",
+        "go_to_realm": "assets/1920x1080/go_to_realm.png"
     },
     "1760x990": {
         "scout": "assets/1760x990/scout.png",
@@ -25,7 +32,13 @@ IMAGE_PATHS = {
         "food": "assets/1760x990/food.png",
         "feed": "assets/1760x990/feed.png",
         "wizard101": "assets/1760x990/wizard101.png",
-        "close": "assets/1760x990/close.png"
+        "close": "assets/1760x990/close.png",
+        "book": "assets/1760x990/book.png",
+        "option": "assets/1760x990/option.png",
+        "realms": "assets/1760x990/realms.png",
+        "next_realm": "assets/1760x990/next_realm.png",
+        "realm_name": "assets/1760x990/realm_name.png",
+        "go_to_realm": "assets/1760x990/go_to_realm.png"
     },
     "1680x1050": {
         "scout": "assets/1680x1050/scout.png",
@@ -34,7 +47,13 @@ IMAGE_PATHS = {
         "food": "assets/1680x1050/food.png",
         "feed": "assets/1680x1050/feed.png",
         "wizard101": "assets/1680x1050/wizard101.png",
-        "close": "assets/1680x1050/close.png"
+        "close": "assets/1680x1050/close.png",
+        "book": "assets/1680x1050/book.png",
+        "option": "assets/1680x1050/option.png",
+        "realms": "assets/1680x1050/realms.png",
+        "next_realm": "assets/1680x1050/next_realm.png",
+        "realm_name": "assets/1680x1050/realm_name.png",
+        "go_to_realm": "assets/1680x1050/go_to_realm.png"
     },
     "1600x900": {
         "scout": "assets/1600x900/scout.png",
@@ -43,7 +62,13 @@ IMAGE_PATHS = {
         "food": "assets/1600x900/food.png",
         "feed": "assets/1600x900/feed.png",
         "wizard101": "assets/1600x900/wizard101.png",
-        "close": "assets/1600x900/close.png"
+        "close": "assets/1600x900/close.png",
+        "book": "assets/1600x900/book.png",
+        "option": "assets/1600x900/option.png",
+        "realms": "assets/1600x900/realms.png",
+        "next_realm": "assets/1600x900/next_realm.png",
+        "realm_name": "assets/1600x900/realm_name.png",
+        "go_to_realm": "assets/1600x900/go_to_realm.png"
     },
     "1366x768": {
         "scout": "assets/1366x768/scout.png",
@@ -52,7 +77,13 @@ IMAGE_PATHS = {
         "food": "assets/1366x768/food.png",
         "feed": "assets/1366x768/feed.png",
         "wizard101": "assets/1366x768/wizard101.png",
-        "close": "assets/1366x768/close.png"
+        "close": "assets/1366x768/close.png",
+        "book": "assets/1366x768/book.png",
+        "option": "assets/1366x768/option.png",
+        "realms": "assets/1366x768/realms.png",
+        "next_realm": "assets/1366x768/next_realm.png",
+        "realm_name": "assets/1366x768/realm_name.png",
+        "go_to_realm": "assets/1366x768/go_to_realm.png"
     },
     "1280x1024": {
         "scout": "assets/1280x1024/scout.png",
@@ -61,7 +92,13 @@ IMAGE_PATHS = {
         "food": "assets/1280x1024/food.png",
         "feed": "assets/1280x1024/feed.png",
         "wizard101": "assets/1280x1024/wizard101.png",
-        "close": "assets/1280x1024/close.png"
+        "close": "assets/1280x1024/close.png",
+        "book": "assets/1280x1024/book.png",
+        "option": "assets/1280x1024/option.png",
+        "realms": "assets/1280x1024/realms.png",
+        "next_realm": "assets/1280x1024/next_realm.png",
+        "realm_name": "assets/1280x1024/realm_name.png",
+        "go_to_realm": "assets/1280x1024/go_to_realm.png"
     },
     "1280x720": {
         "scout": "assets/1280x720/scout.png",
@@ -70,7 +107,13 @@ IMAGE_PATHS = {
         "food": "assets/1280x720/food.png",
         "feed": "assets/1280x720/feed.png",
         "wizard101": "assets/1280x720/wizard101.png",
-        "close": "assets/1280x720/close.png"
+        "close": "assets/1280x720/close.png",
+        "book": "assets/1280x720/book.png",
+        "option": "assets/1280x720/option.png",
+        "realms": "assets/1280x720/realms.png",
+        "next_realm": "assets/1280x720/next_realm.png",
+        "realm_name": "assets/1280x720/realm_name.png",
+        "go_to_realm": "assets/1280x720/go_to_realm.png"
     },
     "1128x634": {
         "scout": "assets/1128x634/scout.png",
@@ -79,7 +122,13 @@ IMAGE_PATHS = {
         "food": "assets/1128x634/food.png",
         "feed": "assets/1128x634/feed.png",
         "wizard101": "assets/1128x634/wizard101.png",
-        "close": "assets/1128x634/close.png"
+        "close": "assets/1128x634/close.png",
+        "book": "assets/1128x634/book.png",
+        "option": "assets/1128x634/option.png",
+        "realms": "assets/1128x634/realms.png",
+        "next_realm": "assets/1128x634/next_realm.png",
+        "realm_name": "assets/1128x634/realm_name.png",
+        "go_to_realm": "assets/1128x634/go_to_realm.png"
     },
     "1024x768": {
         "scout": "assets/1024x768/scout.png",
@@ -88,7 +137,13 @@ IMAGE_PATHS = {
         "food": "assets/1024x768/food.png",
         "feed": "assets/1024x768/feed.png",
         "wizard101": "assets/1024x768/wizard101.png",
-        "close": "assets/1024x768/close.png"
+        "close": "assets/1024x768/close.png",
+        "book": "assets/1024x768/book.png",
+        "option": "assets/1024x768/option.png",
+        "realms": "assets/1024x768/realms.png",
+        "next_realm": "assets/1024x768/next_realm.png",
+        "realm_name": "assets/1024x768/realm_name.png",
+        "go_to_realm": "assets/1024x768/go_to_realm.png"
     },
     "800x600": {
         "scout1": "assets/800x600/scout1.png",
@@ -99,9 +154,15 @@ IMAGE_PATHS = {
         "feed": "assets/800x600/feed.png",
         "wizard101": "assets/800x600/wizard101.png",
         "close": "assets/800x600/close.png",
-        "happiness": "assets/800x600/happiness.png"
+        "book": "assets/800x600/book.png",
+        "option": "assets/800x600/option.png",
+        "realms": "assets/800x600/realms.png",
+        "next_realm": "assets/800x600/next_realm.png",
+        "realm_name": "assets/800x600/realm_name.png",
+        "go_to_realm": "assets/800x600/go_to_realm.png"
     }
 }
+
 
 # Predefined screen resolutions
 RESOLUTIONS = {
@@ -120,6 +181,13 @@ RESOLUTIONS = {
 # Configure logging to show time, log level, and message
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+
+
+
+
+
+
+
 def prompt_for_resolution():
     """
     Prompt the user to select a resolution from the predefined list.
@@ -127,14 +195,14 @@ def prompt_for_resolution():
         str: The selected resolution key.
     """
     print("Select a resolution from the following options:")
-    for index, (resolution, dimensions) in enumerate(RESOLUTIONS.items(), 1):
-        print(f"{index}. {resolution} ({dimensions[0]}x{dimensions[1]})")
-    
+    for index, resolution in enumerate(IMAGE_PATHS.keys(), 1):
+        print(f"{index}. {resolution}")
+
     while True:
         try:
             choice = int(input("Enter the number corresponding to your resolution choice: "))
-            if 1 <= choice <= len(RESOLUTIONS):
-                resolution_key = list(RESOLUTIONS.keys())[choice - 1]
+            if 1 <= choice <= len(IMAGE_PATHS):
+                resolution_key = list(IMAGE_PATHS.keys())[choice - 1]
                 print(f"You selected: {resolution_key}")
                 return resolution_key
             else:
@@ -164,14 +232,13 @@ def search_image(image_path, precision=0.8):
     try:
         screenshot = pya.screenshot()
         screenshot_path = "screenshot.png"
-        screenshot.save(screenshot_path)  # Save screenshot for debugging
+        screenshot.save(screenshot_path)
         logging.debug("Screenshot saved as screenshot.png")
 
-        # Use custom image search function from imgs module
         position = imgs.imagesearch(image_path, screenshot=screenshot, precision=precision)
         logging.debug(f"Image search result: {position}")
 
-        if position[0] != -1:
+        if position and position[0] != -1:
             logging.info(f"Image found at position {position}.")
             return position
         else:
@@ -191,7 +258,7 @@ def click_image(image_path, precision=0.8):
     Returns:
         bool: True if the image was found and clicked, False otherwise.
     """
-    if not image_path or not file_exists(image_path):
+    if not file_exists(image_path):
         logging.error(f"Image path {image_path} is missing or file does not exist.")
         return False
 
@@ -205,8 +272,34 @@ def click_image(image_path, precision=0.8):
     except Exception as e:
         logging.error(f"Error in click_image: {e}")
         return False
-    
 
+def change_realm():
+    """
+    Perform the sequence of clicks to change the realm.
+    """
+    if click_image('assets/800x600/book.png'):
+        t.sleep(1)  # Wait for the menu to open
+        
+        if click_image("assets/800x600/option.png"):
+            t.sleep(1)
+            
+            if click_image("assets/800x600/realms.png"):
+                t.sleep(1)
+                
+                for _ in range(random.randint(1, 5)):
+                    if not click_image("assets/800x600/next_realm.png"):
+                        logging.error("Failed to click 'Next'.")
+                        return
+                
+                t.sleep(1)
+                
+                if click_image("assets/800x600/realm_name.png"):
+                    t.sleep(1)
+                    
+                    if not click_image("assets/800x600/go_to_realm.png"):
+                        logging.error("Failed to click 'Go to Realm'.")
+    else:
+        logging.error("Failed to click the book.")
 
 def perform_sequence(resolution_key):
     """
@@ -215,25 +308,8 @@ def perform_sequence(resolution_key):
         resolution_key (str): The selected screen resolution key.
     """
     image_paths = IMAGE_PATHS.get(resolution_key, {})
-    resolution = RESOLUTIONS.get(resolution_key, (0, 0))
-    width, height = resolution
 
-    def click_center():
-        """Click the center of the screen based on the resolution."""
-        center_x = width // 2
-        center_y = height // 2
-        pya.click(center_x, center_y)
-        logging.info(f"Clicked center of the screen at ({center_x}, {center_y})")
-
-    def check_happiness():
-        """
-        Check for the presence of the happiness image.
-        Returns:
-            bool: True if happiness is detected, False otherwise.
-        """
-        happiness_image_path = image_paths.get("happiness")
-        return happiness_image_path and click_image(happiness_image_path)
-
+    # Function to handle clicking an image and logging its status
     def handle_image(image_key):
         """
         Handle clicking an image and logging its status.
@@ -245,60 +321,53 @@ def perform_sequence(resolution_key):
         image_path = image_paths.get(image_key)
         return click_image(image_path) if image_path else False
 
-    # Step 1: Attempt to click Scout
+    # First, handle scout1 and scout2
     if handle_image("scout1") or handle_image("scout2"):
-        click_center()
-        t.sleep(125)  # Wait for 2 minutes before the next action
-        return
+        t.sleep(125)  # Wait for 2 minutes before proceeding to the next actions
+        return  # Exit after handling scout1 or scout2
 
-    # Step 2: If Scout not found, check for Snack
+    # If scout1 and scout2 are not found, proceed with other actions
     if handle_image("snack"):
         if check_happiness():  # Only click food if happiness is not detected
-            if handle_image("food"):
-                handle_image("feed")
+            handle_image("food")
+        handle_image("feed")
         handle_image("close")
         t.sleep(1)  # Wait 1 second before trying Scout
     else:
         handle_image("pet")
-        return
 
     handle_image("close")
     t.sleep(1)  # Wait 1 second before trying Scout
 
-
+    # Finally, change the realm
+    change_realm()
 
 
 
 def press_key(d_duration=2):
     """
-    Simulate pressing 'D' and 'Ctrl' keys with specified durations.
-    
+    Simulate pressing 'D' key with a specified duration.
     Args:
         d_duration (int): Duration to press the 'D' key in seconds.
-        ctrl_duration (int): Duration to hold down the 'Ctrl' key in seconds.
-    """    
+    """
     keyboard = Controller()
     
     try:
-        # Press and hold 'D' key
         keyboard.press('d')
         logging.info(f"D key pressed down.")
-        
-        # Wait for the duration while 'D' key is held
         t.sleep(d_duration)
-        
-        
+        keyboard.release('d')
+        logging.info(f"D key released.")
     except Exception as e:
-        logging.error(f"Error in press_d_then_ctrl: {e}")
+        logging.error(f"Error in press_key: {e}")
+
 def wait_for_image(image_path, precision=0.8, wait_time=60):
     """
     Wait for an image to appear on the screen with a countdown.
-
     Args:
         image_path (str): Path to the image file.
         precision (float): Precision of the image search.
         wait_time (int): Maximum time to wait for the image in seconds.
-
     Returns:
         bool: True if the image is found, False if timeout occurs.
     """
@@ -318,24 +387,16 @@ def wait_for_image(image_path, precision=0.8, wait_time=60):
             logging.error(f"Timeout waiting for {image_path}.")
             return False
 
-        # Print the countdown
         print(f"\rTime remaining: {remaining_time} seconds", end="")
-        t.sleep(1)  # Wait for 1 second before checking again
-
-    # Note: The above code will never reach this point due to the while loop,
-    # but it's good practice to return something in case the loop is ever exited.
-    return False
-
+        t.sleep(1)
 
 if __name__ == "__main__":
-    # Prompt user for resolution
     resolution_key = prompt_for_resolution()
 
-    # Wait for the "wizard101" image to appear before running the script
     wizard101_image_path = IMAGE_PATHS[resolution_key]["wizard101"]
     if not wait_for_image(wizard101_image_path, precision=0.8):
         logging.error("The 'wizard101' image was not found within the timeout period. Exiting the script.")
-        exit(1)  # Exit the script if 'wizard101' is not found
+        exit(1)
 
     t.sleep(5)  # Initial wait before starting the main loop
 
